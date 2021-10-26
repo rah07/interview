d279aec1f1be8b1b62abea39880d102502083ea7
Rahul Nirwan <rahulnirwan44@gmail.com>
2021-10-26 20:12:26 +0530

    Static Files For Pages

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const app=express();
 
 
 const expressLayouts = require("express-ejs-layouts");
+app.use(express.static("./assets"));
 app.use(expressLayouts);
 
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 // use express router
 app.use("/",require("./routes"));
 
