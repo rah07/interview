a8097c9d82ac5d9bf14f2a90f6c4bc7ee9befd59
Rahul Nirwan <rahulnirwan44@gmail.com>
2021-10-26 18:51:07 +0530

    set view engine

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const app=express();
 // use express router
 app.use("/",require("./routes"));
 
+// set up view engine
+app.set("view engine","ejs");
+app.set("views","./views");
+
 
 
 app.listen(port,function(err){
