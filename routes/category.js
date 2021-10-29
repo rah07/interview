495ac20d2434aa32981114bbaab62ce850043637
Rahul Nirwan <rahulnirwan44@gmail.com>
2021-10-29 18:12:34 +0530

    delete category route and controller file

--- routes/category.js
+++
@@ -1,8 +0,0 @@
-// const express = require("express");
-// const router = express.Router();
-
-// const CategoryController = require("../controllers/category_controller");
-
-// router.post("/create", CategoryController.create);
-
-// module.exports = router;
